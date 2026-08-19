bdfa6b76ec28ad3bc6cc9e76a5fb32ee749ced1d
jacktravolta <juan.espinoza.castro88@gmail.com>
2026-08-18 23:20:56 -0400

    docs: homologacion PDF + fix job encola Reconcile + benchmark 14ms home O(1)

--- app/jobs/update_book_ratings_on_user_ban_job.rb
+++ app/jobs/update_book_ratings_on_user_ban_job.rb
@@ -1,9 +1,14 @@
 class UpdateBookRatingsOnUserBanJob < ApplicationJob
   queue_as :default
+  
   def perform(user_id)
     user = User.find_by(id: user_id)
     return unless user
+    
     book_ids = Review.where(user_id: user.id).distinct.pluck(:book_id)
-    Book.where(id: book_ids).find_each(&:reconcile_valid_ratings!)
+    
+    book_ids.each do |book_id|
+      ReconcileBookRatingJob.perform_later(book_id)
+    end
   end
 end
