61afa5ea2252ab01a64ac290237e4c85d7b90e87
jacktravolta <juan.espinoza.castro88@gmail.com>
2026-08-19 01:04:06 -0400

    timing visible

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,17 @@
-# Seeds idempotentes
+# db/seeds.rb al final
+start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+# ... todo tu código de crear books/users/reviews ...
+
+elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
+
+FileUtils.mkdir_p("tmp")
+File.write("tmp/data_generation_timing.txt", <<~TXT)
+  Books: #{Book.count}
+  Users: #{User.count}
+  Reviews: #{Review.count}
+  Tiempo: #{elapsed.round(2)}s
+  Fecha: #{Time.now}
+TXT
+
+puts "Timing guardado en tmp/data_generation_timing.txt"
